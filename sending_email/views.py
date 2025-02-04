51054b02a6d4cd0f0386f71b9e9f39290c27c420
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-02-04 17:28:28 +0100

    Created the views in sending_email, function post, and function send_emails for sending emails

--- sending_email/views.py
+++ sending_email/views.py
@@ -3,8 +3,9 @@ from django.core.mail import send_mail
 from .serializers import EmailSerializers
 from .models import EmailModels
 from django.conf import settings
-from rest_framework import generics, status, permission
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
+from django.conf import settings
 
 
 # Email views for sending the email.
@@ -13,5 +14,29 @@ class Sendemailview(generics.CreateApiView):
     serializers_class = EmailSerializers
     queryset = EmailModels.objects.all()
     permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializers = EmailSerializers(data=request.data)
+        if serializers.is_valid():
+            emails = serializers.save()
+            self.send_mail(emails)
+            return Response(serializers.data, status=status.HTTP_200_CREATED)
+        return Response(serializers.error, status=status.HTTP_400_BAD_REQUEST)
     
-    
+    def send_emails(self, emails):
+        subject = f'{emails.name} sent a message from your website!'
+        message = (
+            f'Name: {emails.name}\n\n'
+            f'Email: {emails.email_here}\n\n'
+            f'Description: {emails.descriptions}'
+        )
+        my_email = settings.EMAIL_HOST_USER
+        recipient_list = [settings.EMAIL_HOST_USER]
+
+        send_mail(
+            subject,
+            message,
+            my_email,
+            recipient_list,
+            fail_silently=False,
+        )
