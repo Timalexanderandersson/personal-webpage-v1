93abefb5275a3ee942192c4fdeb0fc0f0d3e9f09
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-02-04 17:09:19 +0100

    Created for views in sending_email app

--- sending_email/views.py
+++ sending_email/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from .serializers import EmailSerializers
+from .models import EmailModels
+from django.conf import settings
+from rest_framework import generics, status, permission
+from rest_framework.response import Response
 
-# Create your views here.
+
+# Email views for sending the email.
+
+class Sendemailview(generics.CreateApiView):
+    serializers_class = EmailSerializers
+    queryset = EmailModels.objects.all()
+    permission_classes = [permissions.AllowAny]
+    
+    
