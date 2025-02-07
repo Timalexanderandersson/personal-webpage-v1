978fcd53d7958ce2711650b02e3801c7d1b54dcf
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-02-07 19:02:58 +0100

    created all styles in myprojects.moduyle.css, and fixed text in about me

--- frontend/src/components/Aboutme.js
+++ frontend/src/components/Aboutme.js
@@ -7,7 +7,7 @@ const AboutMe = () => {
         <>
             <div className={styles.aboutmediv}>
                 <div className={styles.newdiv}>
-                    <h1 className='mt-3'>Tim Andersson</h1>
+                    <h1 className='mt-4'>Tim Andersson</h1>
                     <p className='mt-3'>
                         Denna sida är skapad för visa mina projekt,
                         ge dig möjlighet att ladda ner mitt CV och hitta kontaktinformation om du vill samarbeta eller bara vill veta mer.
