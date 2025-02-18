cc22fbe3c8df50dfe73d93f265f6a1cbf5993e22
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-02-18 13:13:53 +0100

    componenet for css styling fading in on all pages, fadeinfix.js, and css in myproject.module.css

---
+++ frontend/src/components/fadeinfix.js
@@ -0,0 +1,33 @@
+
+import { useState, useRef, useEffect } from "react";
+import styles from '../styles/myprojects.module.css';
+
+const IntersectionObserverComponent = ({ children, options }) => {
+    const [isView, setIsView] = useState(false);
+    const targetRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            const [entry] = entries;
+            setIsView(entry.isIntersecting);
+        }, options);
+
+        if (targetRef.current) {
+            observer.observe(targetRef.current);
+        }
+
+        return () => {
+            if (targetRef.current) {
+                observer.unobserve(targetRef.current);
+            }
+        };
+    }, [options]);
+
+    return (
+        <div ref={targetRef} className={`${styles.wowowow} ${isView ? styles.visible : ""}`}>
+            {children}
+        </div>
+    );
+};
+
+export default IntersectionObserverComponent;
