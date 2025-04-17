c981faf9609f51c66d9c975eeddbaa03f34e86fc
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-04-17 18:54:11 +0200

    .

--- backend/settings.py
+++ backend/settings.py
@@ -70,8 +70,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-      'https://my-webpage-v1-309966656ad2.herokuapp.com/',
-        "https://timandersson.dev" , # Lägg till din frontend-domän här
+      'https://my-webpage-v1-309966656ad2.herokuapp.com/',# Lägg till din frontend-domän här
     os.environ["MY-FRONTEND"],
     os.environ["MY-FRONTENDSECOND"],
 ]
